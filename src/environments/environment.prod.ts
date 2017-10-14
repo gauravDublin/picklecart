184b3b4a03a051e0a79346e74114e1e467279dfa
Gaurav Kapoor <gaurav.kapoor2@mail.dcu.ie>
2017-10-15 01:16:34 +0200

    Initialize the shopping cart project

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBfjDUhA1w3cPOlrgubEmRDlvoar6fxTBE",
+    authDomain: "epickle-d8e5e.firebaseapp.com",
+    databaseURL: "https://epickle-d8e5e.firebaseio.com",
+    projectId: "epickle-d8e5e",
+    storageBucket: "epickle-d8e5e.appspot.com",
+    messagingSenderId: "371369279694"
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,16 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBfjDUhA1w3cPOlrgubEmRDlvoar6fxTBE",
+    authDomain: "epickle-d8e5e.firebaseapp.com",
+    databaseURL: "https://epickle-d8e5e.firebaseio.com",
+    projectId: "epickle-d8e5e",
+    storageBucket: "epickle-d8e5e.appspot.com",
+    messagingSenderId: "371369279694"
+  }
+};
