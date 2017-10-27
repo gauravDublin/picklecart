5918fa9b7581abe7640210883ae02d9660f57532
Gaurav Kapoor <gaurav.kapoor2@mail.dcu.ie>
2017-10-28 00:20:17 +0200

    URL after login is saved into localstorage

--- src/app/auth-guard.service.ts
+++ src/app/auth-guard.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { CanActivate } from '@angular/router';
+import { CanActivate, RouterStateSnapshot } from '@angular/router';
 import { Router } from '@angular/router';
 import {AuthService} from "./auth.service";
 import 'rxjs/add/operator/map';
@@ -9,11 +9,13 @@ export class AuthGuard implements CanActivate {
 
   constructor(private auth: AuthService, private router: Router) { }
 
-  canActivate() {
+  canActivate(route, state: RouterStateSnapshot) {
     return this.auth.user$.map(user => {
       if(user) return true;
 
-      this.router.navigate(['/login']);
+      this.router.navigate(['/login'], { queryParams: {
+        returnUrl : state.url
+      }});
       return false;
     });
   }
