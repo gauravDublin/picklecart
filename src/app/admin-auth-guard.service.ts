2d2fb178e79587319c815d8083d67921748ce683
Gaurav Kapoor <gaurav.kapoor2@mail.dcu.ie>
2017-10-28 18:52:06 +0200

    Admin Login completed

--- src/app/admin-auth-guard.service.ts
+++ src/app/admin-auth-guard.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { CanActivate, RouterStateSnapshot } from '@angular/router';
 import {AuthService} from "./auth.service";
 import {UserService} from "./user.service";
-import 'rxjs/add/operator/switchMap';
 import 'rxjs/add/operator/map';
 import {Observable} from "rxjs/Observable";
 
--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -5,6 +5,7 @@ import {Observable} from "rxjs/Observable";
 import { ActivatedRoute } from '@angular/router';
 import {AppUser} from "./models/app-user";
 import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/observable/of';
 import {UserService} from "./user.service";
 
 @Injectable()
@@ -27,6 +28,9 @@ export class AuthService {
 
   get appUser$(): Observable<AppUser> {
     return this.user$
-      .switchMap(user => this.userService.get(user.uid));
+      .switchMap(user => {
+        if(user) return this.userService.get(user.uid);
+        return Observable.of(null);
+      });
   }
 }
