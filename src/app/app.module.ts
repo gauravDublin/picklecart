0bb8d8c5794d4ba304787d91c38382562bae7a1f
Gaurav Kapoor <gaurav.kapoor2@mail.dcu.ie>
2017-11-01 01:44:44 +0100

    New Product page added for Admin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,6 +22,8 @@ import { AuthService } from './auth.service';
 import {AuthGuard} from "./auth-guard.service";
 import {UserService} from "./user.service";
 import {AdminAuthGuard} from "./admin-auth-guard.service";
+import { ProductFormComponent } from './admin/product-form/product-form.component';
+import {CategoryService} from "./category.service";
 
 @NgModule({
   declarations: [
@@ -36,6 +38,7 @@ import {AdminAuthGuard} from "./admin-auth-guard.service";
     AdminProductsComponent,
     AdminOrdersComponent,
     LoginComponent,
+    ProductFormComponent,
   ],
   imports: [
     BrowserModule,
@@ -53,6 +56,7 @@ import {AdminAuthGuard} from "./admin-auth-guard.service";
       {path:'my/orders', component:MyOrdersComponent, canActivate: [AuthGuard]},
 
       {path:'admin/products', component:AdminProductsComponent, canActivate: [AuthGuard, AdminAuthGuard]},
+      {path:'admin/products/new', component:ProductFormComponent, canActivate: [AuthGuard, AdminAuthGuard]},
       {path:'admin/orders', component:AdminOrdersComponent, canActivate: [AuthGuard, AdminAuthGuard]},
     ]),
     NgbModule.forRoot()
@@ -61,7 +65,8 @@ import {AdminAuthGuard} from "./admin-auth-guard.service";
     AuthService,
     AuthGuard,
     UserService,
-    AdminAuthGuard
+    AdminAuthGuard,
+    CategoryService
   ],
   bootstrap: [AppComponent]
 })
